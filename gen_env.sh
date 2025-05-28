c5f595a7f8b18842bf38a557d9286f97479ae5dc
DynamicCake <128557765+DynamicCake@users.noreply.github.com>
2025-05-27 20:51:05 -1000

    Make gen_env.sh give valid keys
    Also make some tweaks

--- gen_env.sh
+++ gen_env.sh
@@ -2,38 +2,33 @@
 
 ENV_FILE=".env"
 echo "This utility will walk you through creating a docker compose $ENV_FILE file."
-echo "If you are not sure, follow what the () say"
 echo ""
-generate_random_string() {
-  openssl rand "$1" | base64 -w 0 | tr '+/' '-_'
-}
 
-read -p "PostgreSQL Password (a strong password): " DFQ_POSTGRES_PASSWORD
-read -p "Host domain (e.g. example.com): " DFQ_HOST
+read -p "PostgreSQL Password: " DFQ_POSTGRES_PASSWORD
+read -p "Host domain e.g. example.com: " DFQ_HOST
 
-read -p "JWT key (leave blank to generate): " DFQ_JWT_KEY
-if [ -z "$DFQ_JWT_KEY" ]; then
-  DFQ_JWT_KEY=$(generate_random_string 64)
-  echo "Generated jwt key: $DFQ_JWT_KEY"
-fi
-
-read -p "Secret key (leave blank to generate): " DFQ_SECRET_KEY
-if [ -z "$DFQ_SECRET_KEY" ]; then
-  DFQ_SECRET_KEY=$(generate_random_string 32)
-  echo "Secret key: $DFQ_SECRET_KEY"
-fi
-
-read -p "Enter DFQ_PORT (8080): " DFQ_PORT
+read -p "Enter port (default: 8080): " DFQ_PORT
 if [ -z "$DFQ_PORT" ]; then
   DFQ_PORT=8080
-  echo "Using default DFQ_PORT: $DFQ_PORT"
+fi
+
+read -p "Secret key (default: generate): " DFQ_SECRET_KEY
+if [ -z "$DFQ_SECRET_KEY" ]; then
+  DFQ_SECRET_KEY=$(openssl genpkey -algorithm ED25519 | awk '/^-----BEGIN PRIVATE KEY-----/{p=1;next}/^-----END PRIVATE KEY-----/{p=0}p')
+fi
+
+if [ -f $ENV_FILE ]; then
+  YELLOW='\033[1;33m'
+  NC='\033[0m' # No Color
+  echo -e "${YELLOW}WARNING: $ENV_FILE EXISTS!"
+  echo -e "The file isn't overriden yet. Press Ctrl+C to exit early${NC}"
+  read -p "Press enter to proceed..."
 fi
 
 # Write env file
 cat << EOF > "$ENV_FILE"
 DFQ_POSTGRES_PASSWORD="$DFQ_POSTGRES_PASSWORD"
 DFQ_HOST="$DFQ_HOST"
-DFQ_JWT_KEY="$DFQ_JWT_KEY"
 DFQ_SECRET_KEY="$DFQ_SECRET_KEY"
 DFQ_PORT="$DFQ_PORT"
 
